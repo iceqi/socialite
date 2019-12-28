b42058f9aaa83558ad378296c8334d38305a7b32
Atsuji Okuyama <okuyama@workgroup.jp>
2019-12-28 09:14:24 +0900

    Line API v2.1対応

--- src/Two/LineProvider.php
+++ src/Two/LineProvider.php
@@ -5,47 +5,109 @@ namespace RingoProject\Socialite\Two;
 use Laravel\Socialite\Two\AbstractProvider;
 use Laravel\Socialite\Two\ProviderInterface;
 use Laravel\Socialite\Two\User;
+use Laravel\Socialite\Two\InvalidStateException;
+use GuzzleHttp\ClientInterface;
+use Illuminate\Support\Arr;
 
 class LineProvider extends AbstractProvider implements ProviderInterface
 {
-	protected function getAuthUrl($state)
-	{
-		return $this->buildAuthUrlFromBase('https://access.line.me/dialog/oauth/weblogin', $state);
-	}
+    protected $scopeSeparator = ' ';
 
-	protected function getTokenUrl()
-	{
-		return 'https://api.line.me/v2/oauth/accessToken';
-	}
+    protected $scopes = [
+        'openid',
+        'profile',
+        'email',
+    ];
 
-	protected function getUserByToken($token)
-	{
-		$response = $this->getHttpClient()->get('https://api.line.me/v2/profile', [
-			'headers' => [
-				'X-Line-ChannelToken' => $token,
-			],
-		]);
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://access.line.me/oauth2/v2.1/authorize', $state);
+    }
 
-		return json_decode($response->getBody(), true);
-	}
+    protected function getTokenUrl()
+    {
+        return 'https://api.line.me/oauth2/v2.1/token';
+    }
 
-	protected function mapUserToObject(array $user)
-	{
-		return (new User())->setRaw($user)->map([
-			'id'     => $user['userId'],
-			'name'   => $user['displayName'],
-			'avatar' => $user['pictureUrl'],
-		]);
-	}
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'     => $user['userId'],
+            'name'   => $user['displayName'],
+            'avatar' => $user['pictureUrl'],
+        ]);
+    }
 
-	protected function getTokenFields($code)
-	{
-		return [
-			'client_id'     => $this->clientId,
-			'client_secret' => $this->clientSecret,
-			'code'          => $code,
-			'redirect_uri'  => $this->redirectUrl,
-			'grant_type'    => 'authorization_code',
-		];
-	}
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.line.me/v2/profile', [
+            'headers' => [
+                //'X-Line-ChannelToken' => $token,
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    public function user()
+    {
+        if ($this->hasInvalidState()) {
+            throw new InvalidStateException;
+        }
+
+        $response = $this->getAccessTokenResponse($this->getCode());
+
+        $postKey = (version_compare(ClientInterface::VERSION, '6') === 1) ? 'form_params' : 'body';
+
+        $response2 = $this->getHttpClient()->post('https://api.line.me/oauth2/v2.1/verify', [
+            'headers' => ['Accept' => 'application/json'],
+            $postKey => [
+                'id_token' => Arr::get(
+                    $response,
+                    'id_token'
+                ), 'client_id' => $this->clientId
+            ],
+        ]);
+        $verify = json_decode($response2->getBody(), true);
+
+        $user = $this->mapUserToObject($this->getUserByToken(
+            $token = Arr::get($response, 'access_token')
+        ));
+        $user->setRaw($verify)->map([
+            'email' => isset($verify['email']) ? $verify['email'] : null,
+        ]);
+
+        return $user->setToken($token)
+            ->setRefreshToken(Arr::get($response, 'refresh_token'))
+            ->setExpiresIn(Arr::get($response, 'expires_in'));
+    }
+
+    protected function getCodeFields($state = null)
+    {
+        $fields = [
+            'client_id' => $this->clientId,
+            'redirect_uri' => $this->redirectUrl,
+            'scope' => $this->formatScopes($this->getScopes(), $this->scopeSeparator),
+            'response_type' => 'code',
+            //'prompt' => 'consent',
+        ];
+
+        if ($this->usesState()) {
+            $fields['state'] = $state;
+        }
+
+        return array_merge($fields, $this->parameters);
+    }
+
+    protected function getTokenFields($code)
+    {
+        return [
+            'client_id'     => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'code'          => $code,
+            'redirect_uri'  => $this->redirectUrl,
+            'grant_type'    => 'authorization_code',
+        ];
+    }
 }
